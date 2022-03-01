aa115269ed71da8ee9596c0e46d3c3311a52e7cb
ji21 <jeffreyip54@gmail.com>
2022-03-01 06:17:21 +0000

    user model

--- main.go
+++ main.go
@@ -7,6 +7,8 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+var isStreaming = false
+
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
--- models.go
+++ models.go
@@ -1,6 +1,13 @@
 package main
 
+import "github.com/gorilla/websocket"
+
 type WSMessage struct {
 	Type string `json:"type"`
 	Data string `json:"data"`
+}
+
+type User struct {
+	ID   int
+	conn *websocket.Conn
 }
